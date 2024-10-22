9ced4834b0087fdded034af2a4d890761722cad1
mouadHILLALI <mauadtoto001@gmail.com>
2024-10-22 20:13:22 +0100

    web configuration

---
+++ src/main/java/insurancetracker/insurancetracker/controller/TestController.java
@@ -0,0 +1,16 @@
+package insurancetracker.insurancetracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/get")
+    public String get() {
+        System.out.println("GET request received for /test/get");
+        return "test";
+    }
+}
